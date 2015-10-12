db7c45b36ec0e1b32df715b8b3dc1f8ac9b97c0f
Seth Kranzler <sethkranzler@gmail.com>
2015-10-11 22:27:38 -0400

    Adds sound generation extension test

---
+++ sound-track/scripts/content_script.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+	var random = Math.random();
+
+	if(random < 0.5){
+		var reverb = new Tone.Freeverb().toMaster();
+		var glass = new Tone.FMGlass().connect(reverb);
+
+		glass.triggerAttackRelease("A4", "4m");
+		glass.triggerAttackRelease("C2", "4m", "+4m");
+		glass.triggerAttackRelease("A1", "4m", "+8m");
+		glass.triggerAttackRelease("E3", "4m", "+12m");
+		glass.triggerAttackRelease("A1", "4m", "+16m");
+	} else {
+		var bell = new Tone.FMBell().toMaster();
+
+		bell.modulationIndex.setValueCurveAtTime("0", 1,"4n");
+		bell.triggerAttackRelease("A4", "1m");
+
+		bell.modulationIndex.setValueAtTime(10, "+4n");
+		bell.modulationIndex.setValueCurveAtTime("+4n", 1,"4n");
+		bell.triggerAttackRelease("C5", "1m", "+4n");
+
+		bell.modulationIndex.setValueAtTime(10, "+2n");
+		bell.modulationIndex.setValueCurveAtTime("+2n", 1,"4n");
+		bell.triggerAttackRelease("G5", "1m", "+2n");
+	}
+};
